195b3cbdab8b5665f5e9b11944cd4d1916a27b66
abdul hadi <abdulhadiqureashi10@gamil.com>
2024-02-07 09:11:46 +0530

    push

---
+++ src/exam/person.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class person {
+	private String name;
+	private int age;
+	public person(String name, int age) {
+		this.name=name;
+		this.age=age;
+	}
+	public String getName() {
+		return name;
+	}
+
+}
